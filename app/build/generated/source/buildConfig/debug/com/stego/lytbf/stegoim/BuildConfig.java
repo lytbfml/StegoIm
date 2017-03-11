162b569542d7585073b5175bfdf515011594ac12
YWGAMING\Yangxiao <lytbfml@hotmail.com>
2017-03-11 05:37:45 -0600

    build files

---
+++ app/build/generated/source/buildConfig/debug/com/stego/lytbf/stegoim/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.stego.lytbf.stegoim;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.stego.lytbf.stegoim";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/build/intermediates/ndkBuild/debug/obj/local/armeabi/objs-debug/F5Buffers/info_guardianproject_f5android_plugins_f5_F5Buffers.o.d
@@ -0,0 +1,5 @@
+E:/TOP/Code/StegoIm/app/build/intermediates/ndkBuild/debug/obj/local/armeabi/objs-debug/F5Buffers/info_guardianproject_f5android_plugins_f5_F5Buffers.o: \
+  E:/TOP/Code/StegoIm/app/src/main/jni/info_guardianproject_f5android_plugins_f5_F5Buffers.cpp \
+  E:/TOP/Code/StegoIm/app/src/main/jni/info_guardianproject_f5android_plugins_f5_F5Buffers.h
+
+E:/TOP/Code/StegoIm/app/src/main/jni/info_guardianproject_f5android_plugins_f5_F5Buffers.h:
